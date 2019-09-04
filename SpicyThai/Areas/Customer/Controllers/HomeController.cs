3a2935d910eb72a968c3fb8706b109e4f74f7b73
Dalton Larsen <daltonlarsen@mail.weber.edu>
2019-09-04 08:39:02 -0600

    Add project files.

---
+++ SpicyThai/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SpicyThai.Models;
+
+namespace SpicyThai.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Area("Customer")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
