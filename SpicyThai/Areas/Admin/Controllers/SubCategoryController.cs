119dfda09d7bc063032248360e1217fe4a7ad20c
Dalton Larsen <daltonlarsen@mail.weber.edu>
2019-09-16 12:41:06 -0600

    9/16 Added ViewModels and extensions. Need to fix some errors

---
+++ SpicyThai/Areas/Admin/Controllers/SubCategoryController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SpicyThai.Data;
+using SpicyThai.Models;
+using SpicyThai.Models.ViewModels;
+
+namespace SpicyThai.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class SubCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public SubCategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var subCategories = await _db.SubCategory.Include(s=>s.Category).ToListAsync();
+            return View(subCategories);
+        }
+        
+        //Get Create
+        public async Task<IActionResult> Create()
+        {
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync(),
+                SubCategory = new Models.SubCategory,
+                SubCategoryList = await _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).Distinct().ToListAsync()
+            };
+
+            return View(model);
+        }
+    }
+}
