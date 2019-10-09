f951962456745e72a62d55cc4ac306dafaebdaa5
Dalton Larsen <daltonlarsen@mail.weber.edu>
2019-10-09 15:32:03 -0600

    10/9 Made the coupons appear and menu items show up. Need to fix category name showing up in coupon banner.

--- SpicyThai/Areas/Customer/Controllers/HomeController.cs
+++ SpicyThai/Areas/Customer/Controllers/HomeController.cs
@@ -1,15 +1,38 @@
 ﻿using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SpicyThai.Data;
 using SpicyThai.Models;
+using SpicyThai.Models.ViewModels;
 
 namespace SpicyThai.Controllers
 {
     public class HomeController : Controller
     {
-        [Area("Customer")]
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+
+        [Area("Customer")]
+        public async Task<IActionResult> Index()
+        {
+            IndexViewModel IndexViewModel = new IndexViewModel()
+            {
+                MenuItem = await _db.MenuItem
+                                    .Include(m => m.Category)
+                                    .Include(s => s.SubCategory)
+                                    .ToListAsync(),
+                Coupon = await _db.Coupon
+                                  .Where(c=>c.IsActive)
+                                  .ToListAsync(),
+                Category = await _db.Category.ToListAsync()
+            };
+            return View(IndexViewModel);
         }
 
         public IActionResult Privacy()
